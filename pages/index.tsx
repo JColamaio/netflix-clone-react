a9885d6fb87b50a43c3f026206a896ed0e83ba24
Jonathan <jonathan_colamaio@solutionbox.com.ar>
2023-04-12 09:44:04 -0300

    feat: Billboard component complete

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import { getSession } from "next-auth/react"
 import Navbar from "@/components/Navbar";
 import Billboard from "@/components/Billboard";
 
-
 export async function getServerSideProps(context: NextPageContext){
   const session = await getSession(context);
 
@@ -25,7 +24,7 @@ export default function Home() {
   return (
     <>
       <Navbar/>
-      <Billboard />
+      <Billboard/>
     </>
   )
 }
